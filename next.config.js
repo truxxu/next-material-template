6fc194f4fd1a2f45c2303483450a952d121cbd90
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-08-22 10:56:04 -0500

    added login page

--- next.config.js
+++ next.config.js
@@ -3,18 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   modularizeImports: {
-    '@mui/icons-material': {
-      transform: '@mui/icons-material/{{member}}',
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
     },
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'source.unsplash.com',
-        port: '',
-        pathname: '/random',
-      },
+      // {
+      //   protocol: 'https',
+      //   hostname: 'source.unsplash.com',
+      //   port: '',
+      //   pathname: '/random',
+      // },
     ],
   },
 };
